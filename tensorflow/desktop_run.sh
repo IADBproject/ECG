c765f75e44033a92d9be5bbb8d8d9eb95bd14ff2
Ziqing Du <48395791+ZiqingDu@users.noreply.github.com>
2019-06-24 18:38:38 +0200

    Create desktop_run.sh

---
+++ tensorflow/desktop_run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+###########################################################################
+#                                                                         #
+# enerGyPU for monitoring performance and power consumption on Multi-GPU  #
+#                                                                         #
+###########################################################################
+
+# enerGyPU_run.sh
+# Execution steps and the application libraries are declared.
+###########################################################################
+
+
+# Global workload parameters
+# For the sample matrixMul the dimensions of A & B matrices must be equal.
+DIM=1024
+nGPU=1
+
+# Location of the power consumption measures
+Dir=./../enerGyPU/testbed/
+HOST=$(hostname)
+APP="tensorflow"
+DATA=`date +%Y%m%d%H%M`
+ARGV=$HOST-$APP-$DIM-$nGPU-$DATA
+mkdir $Dir/$ARGV
+mkdir output
+
+# Executes the enerGyPU_record.sh
+./../enerGyPU/dataCapture/enerGyPU_record.sh $Dir $ARGV &
+
+# Add path of the application and libraries necessaries
+python3.6 tensorflow.py
+
+mv output $Dir/$ARGV/
+
+kill %1
