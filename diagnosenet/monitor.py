6b3a89e0e8dfae23ec23c71959de13277f66ed9c
jagh <jagh1729@gmail.com>
2019-06-25 11:18:12 +0200

    Deactivated testbed-ID encryption

--- diagnosenet/monitor.py
+++ diagnosenet/monitor.py
@@ -140,7 +140,8 @@ class Testbed(Metrics):
 
         datetime = strftime("%Y%m%d%H%M%S", gmtime())
         exp_id=str(self.data.dataset_name)+"-"+str(self.model.__class__.__name__)+"-"+str(self.platform_name)+"-"+str(datetime)
-        hash_id = hashlib.sha256(exp_id.encode('utf-8')).hexdigest()
+        #hash_id = hashlib.sha256(exp_id.encode('utf-8')).hexdigest()
+        hash_id = exp_id
 
         return hash_id
 
