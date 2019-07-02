960da5acc9095d43a1857a0dbd3b01f1510ea429
Ziqing Du <48395791+ZiqingDu@users.noreply.github.com>
2019-07-02 14:52:53 +0200

    Update mpimain.py

--- tfwithMPI/mpimain.py
+++ tfwithMPI/mpimain.py
@@ -42,7 +42,8 @@ def localdata(filebed="./../input/",sync=1,lr=0.0001,epochs = 15,batch_size = 8)
     size = comm.Get_size()
     rank = comm.Get_rank()
     myhost = MPI.Get_processor_name()
-   
+    model = CNNGraph(input_size_1=1300,input_size_2=1, output_size=4,
+                        learning_rate=lr)
     if rank==0:
         modeling = mastermain((size-1),batch_size,lr=lr,mode=0)
         print("epochs:",epochs,file=modeling.main_file)
@@ -54,15 +55,15 @@ def localdata(filebed="./../input/",sync=1,lr=0.0001,epochs = 15,batch_size = 8)
         ytrain_name=filebed+"train/ytrain-"+str(rank)+".npy"
         yval_name=filebed+"val/yval-"+str(rank)+".npy"
         ytest_name=filebed+"test/ytest-"+str(rank)+".npy"
-        model = CNNGraph(input_size_1=1300,input_size_2=1, output_size=4,
-                        learning_rate=lr)
         modeling=WorkerModeling(model,batch_size)
-        modeling.model.graph()
+        
         modeling.data(xtrain_name,ytrain_name,xval_name,yval_name,xtest_name,ytest_name)
+    modeling.model=model
+    modeling.model.graph()
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     fit = time.time()
-    with tf.Session(config=config, graph=graph) as sess:
+    with tf.Session(config=config, graph=modeling.model.cnn_graph) as sess:
             init = tf.group(tf.global_variables_initializer(),
                         tf.local_variables_initializer())
             sess.run(init)
@@ -167,111 +168,117 @@ def masterdata(sync=1,lr=0.0001,epochs = 15,batch_size = 8):
 
         print("epochs:",epochs,file=modeling.main_file)
         print("ECG MPI  with threads",(size),file=modeling.main_file)
+    model = CNNGraph(input_size_1=1300,input_size_2=1, output_size=4,
+                        learning_rate=lr)
     if rank==0:
         for i in range(1, size):
             comm.send([train_step,val_step,test_step], dest=i)
     else:
-        model = CNNGraph(input_size_1=1300,input_size_2=1, output_size=4,
-                        learning_rate=lr)
         modeling=WorkerModeling(model,batch_size)
         modeling.model.graph()
         train_step,val_step,test_step=comm.recv(source=0)
+    modeling.model=model
+    modeling.model.graph()
     fit = time.time()
+    with tf.Session(config=config, graph=modeling.model.cnn_graph) as sess:
+            init = tf.group(tf.global_variables_initializer(),
+                        tf.local_variables_initializer())
+            sess.run(init)
+            saver = tf.train.Saver()
+            for e in range(epochs):
+                sub_time = time.time()
+                acc,loss,val_acc,val_loss=0,0,0,0
 
-    for e in range(epochs):
-        sub_time = time.time()
+                for s in range(train_step):
+                    if rank==0:
+                        data,label=next(train_next_batch_gen)
+                        for i in range(1,size):
+                            k=i*batch_size
+                            comm.send([data[(i-1)*batch_size:k],label[(i-1)*batch_size:k]], dest=i)
+                    else:
+
+                        data,label=comm.recv(source=0)
+                        grads,losst,acct=sess.run([modeling.model.cnn_grad_op,modeling.model.cnn_loss,modeling.model.accuracy], 
+                                            feed_dict={X: data, Y: label, is_training: True})
+                        acc+=acct/modeling.train_step
+                        loss+=losst/modeling.train_step
+
+                if rank==0:
+                    w=[]
+                    for i in range(1, size):
+                        w.append(comm.recv())
+                    modeling.average_gradients(w)
+                else:
+                    update_weights =  grads
+                    comm.send(update_weights, dest=0)
 
 
-        for s in range(train_step):
+                ####validation
+                if rank==0:
+                    for i in range(1, size):
+                        comm.send(modeling.model_weights, dest=i)
+                else:
+                    modeling.model_weights = comm.recv(source=0)
+                    modeling.read(True)
+
+                val_loss,val_acc=0,0
+                for s in range(val_step):
+                    if rank==0:
+                        data,label=next(val_next_batch_gen)
+                        for i in range(1,size):
+                            k=i*batch_size
+                            comm.send([data[(i-1)*batch_size:k],label[(i-1)*batch_size:k]], dest=i)
+                    else:
+                        data,label=comm.recv(source=0)
+                        losst,acct=sess.run([modeling.model.cnn_loss,modeling.model.accuracy], 
+                                        feed_dict={X: data, Y: label, is_training: True})
+                        val_acc+=acct/modeling.val_step
+                        val_loss+=losst/modeling.val_step
+
+                if rank==0:
+                    w=[]
+                    for i in range(1, size):
+                        w.append(comm.recv(source=i))
+                    modeling.update(w,sub_time,e)
+                else:
+                    comm.send([val_loss,val_acc,loss,acc], dest=0)
+                    modeling.training_track.append((e + 1,loss, val_loss, acc, val_acc, np.round(sub_time, decimals=4))) 
+                    if val_loss < modeling.best_validation_loss:
+                        modeling.best_validation_loss = val_loss
+                        modeling.last_improvement = e+1 
+            end = time.time()
+
+            ####testing
             if rank==0:
-                data,label=next(train_next_batch_gen)
-                for i in range(1,size):
-                    k=i*batch_size
-                    comm.send([data[(i-1)*batch_size:k],label[(i-1)*batch_size:k]], dest=i)
+                print("training time :",end-fit)
+                modeling.training_time=end-fit
+                for i in range(1, size):
+                    comm.send(modeling.best_model_weights, dest=i)
             else:
+                modeling.best_model_weights = comm.recv(source=0)
+                modeling.read(False)
 
-                data,label=comm.recv(source=0)
-                grads,losst,acct=sess.run([modeling.model.cnn_grad_op,modeling.model.cnn_loss,modeling.model.accuracy], 
-                                    feed_dict={X: data, Y: label, is_training: True})
-                acc+=acct/modeling.train_step
-                loss+=losst/modeling.train_step
+            p,t=[],[]
+            for s in range(test_step):
+                if rank==0:
+                    data,label=next(test_next_batch_gen)
+                    for i in range(1,size):
+                        k=i*batch_size
+                        comm.send([data[(i-1)*batch_size:k],label[(i-1)*batch_size:k]], dest=i)
+                else:
+                    data,label=comm.recv(source=0)
+                    pred=sess.run(modeling.model.projection_1hot, 
+                                            feed_dict={X: data, is_training: False})
+                    modeling.pred.append(pred)
+                    modeling.label.append(label)
 
-        if rank==0:
-            w=[]
-            for i in range(1, size):
-                w.append(comm.recv())
-            modeling.average_gradients(w)
-        else:
-            update_weights =  grads
-            comm.send(update_weights, dest=0)
-
-
-        ####validation
-        if rank==0:
-            for i in range(1, size):
-                comm.send(modeling.model_weights, dest=i)
-        else:
-            modeling.model_weights = comm.recv(source=0)
-            modeling.read(True)
-
-        val_loss,val_acc=0,0
-        for s in range(val_step):
-            if rank==0:
-                data,label=next(val_next_batch_gen)
-                for i in range(1,size):
-                    k=i*batch_size
-                    comm.send([data[(i-1)*batch_size:k],label[(i-1)*batch_size:k]], dest=i)
-            else:
-                data,label=comm.recv(source=0)
-                losst,acct=sess.run([modeling.model.cnn_loss,modeling.model.accuracy], 
-                                feed_dict={X: data, Y: label, is_training: True})
-                val_acc+=acct/modeling.val_step
-                val_loss+=losst/modeling.val_step
-
-        if rank==0:
-            w=[]
-            for i in range(1, size):
-                w.append(comm.recv(source=i))
-            modeling.update(w,sub_time,e)
-        else:
-            comm.send([val_loss,val_acc,loss,acc], dest=0)
-            modeling.training_track.append((e + 1,loss, val_loss, acc, val_acc, np.round(sub_time, decimals=4))) 
-            if val_loss < modeling.best_validation_loss:
-                modeling.best_validation_loss = val_loss
-                modeling.last_improvement = e+1 
-    end = time.time()
-
-    ####testing
-    if rank==0:
-        print("training time :",end-fit)
-        modeling.training_time=end-fit
-        for i in range(1, size):
-            comm.send(modeling.best_model_weights, dest=i)
-    else:
-        modeling.best_model_weights = comm.recv(source=0)
-        modeling.read(False)
-
-    p,t=[],[]
-    for s in range(test_step):
-        if rank==0:
-            data,label=next(test_next_batch_gen)
-            for i in range(1,size):
-                k=i*batch_size
-                comm.send([data[(i-1)*batch_size:k],label[(i-1)*batch_size:k]], dest=i)
-        else:
-            data,label=comm.recv(source=0)
-            pred=sess.run(modeling.model.projection_1hot, 
-                                    feed_dict={X: data, is_training: False})
-            modeling.pred.append(pred)
-            modeling.label.append(label)
-
-        if rank==0:
-            for i in range(1, size):
-                tmp1,tmp2=comm.recv(source=i)
-                p.append(tmp1)
-                t.append(tmp2)
-        else:
-            comm.send([pred,label], dest=0)
+                if rank==0:
+                    for i in range(1, size):
+                        tmp1,tmp2=comm.recv(source=i)
+                        p.append(tmp1)
+                        t.append(tmp2)
+                else:
+                    comm.send([pred,label], dest=0)
 
 
     #####output
