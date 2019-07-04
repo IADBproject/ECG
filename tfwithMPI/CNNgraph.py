fixed the gradient updating problem

tfwithMPI/CNNgraph.py
@@ -93,7 +93,7 @@ class CNNGraph:
             self.cnn_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.projection, labels=self.Y))
             self.adam_op = tf.train.AdamOptimizer(self.learning_rate)
             self.cnn_grad_op = self.adam_op.compute_gradients(self.cnn_loss)
-            
+            self.sub_grad_op = self.adam_op.minimize(self.cnn_loss)             
             #self.grad_placeholder = [(tf.placeholder("float", shape=gradt[1].get_shape()), gradt[1]) for gradt in self.cnn_grad_op]
 
             #self.apply_op = self.adam_op.apply_gradients(self.grad_placeholder)